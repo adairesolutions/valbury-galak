b10ac98420535665f828d12819606d736621302d
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-09-10 15:25:56 +0700

    updates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ moment.updateLocale('en', {
   ]
 });
 
-async function RealDate() {
+async function FetchFileName() {
   const url = "https://timeapi.io/api/time/current/zone?timeZone=Asia%2FJakarta";
   try {
     const response = await fetch(url);
@@ -16,22 +16,16 @@ async function RealDate() {
       throw new Error(`Response status: ${response.status}`);
     }
     const json = await response.json();
-    console.log(new Date(json['dateTime']));
+    datenow = new Date(json['dateTime']);
   } catch (error) {
     console.error(error.message);
-    // console.error(error.message + ", Re Run");
-    // RealDate();
-    // return;
   }
-}
 
-function FetchFileName() {
-  var date = new Date();
+  var date = datenow;
   var dateclean = moment(date).locale('en').format('DD');
   var monthclean = moment(date).locale('en').format('MMM');
   var yearclean = moment(date).locale('en').format('YYYY');
   var outlookname = dateclean + '_' + monthclean + '_' + yearclean + '_' + 'DAILY_MARKET_OUTLOOK_';
-
   const path = require('path');
   const filePath = path.join(__dirname, './market-outlook/' + outlookname + '.pdf');
 
@@ -42,31 +36,49 @@ function FetchFileName() {
     }
 
     var pdfLength = JSON.stringify(pages.slice(3, -3)).length;
-    console.log('Length: ' + JSON.stringify(pages.slice(3, -3)).length);
+    var pdfText = JSON.stringify(pages.slice(3, -3));
+    // console.log(pdfText);
 
     if (pdfLength = 872) {
       // EURUSD
-      console.log('eurusd');
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(2, 10).toLocaleLowerCase());
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(18, 25));
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(71, 78));
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(125, 132));
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(178, 185));
-      // GBPUSD
-      console.log('gbpusd');
+      var eurusd_signals = [];
+      var signaldate = moment(date).locale('id').format('MM/DD/YY').toString();
+      var signalorder = JSON.stringify(pages.slice(3, -3)).substring(2, 6).toLocaleLowerCase();
+      var signalprice = JSON.stringify(pages.slice(3, -3)).substring(18, 25);
+      var signalsl = JSON.stringify(pages.slice(3, -3)).substring(71, 78);
+      var signaltp1 = JSON.stringify(pages.slice(3, -3)).substring(125, 132);
+      var signaltp2 = JSON.stringify(pages.slice(3, -3)).substring(178, 185);
+      var eursignalobj = {
+        'date': signaldate,
+        'order': signalorder,
+        'price': signalprice,
+        'stoploss': signalsl,
+        'takeprofit1': signaltp1,
+        'takeprofit2': signaltp2,
+      }
+      eurusd_signals.push(eursignalobj);
+      console.log(eurusd_signals);
     } else if (pdfLength = 879) {
       // EURUSD
-      console.log('eurusd');
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(2, 10).toLocaleLowerCase());
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(19, 26));
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(72, 79));
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(126, 133));
-      console.log(JSON.stringify(pages.slice(3, -3)).substring(180, 187));
-      // GBPUSD
-      console.log('gbpusd');
+      var eurusd_signals = [];
+      var signaldate = moment(date).locale('id').format('MM/DD/YY').toString();
+      var signalorder = JSON.stringify(pages.slice(3, -3)).substring(2, 6).toLocaleLowerCase();
+      var signalprice = JSON.stringify(pages.slice(3, -3)).substring(18, 25);
+      var signalsl = JSON.stringify(pages.slice(3, -3)).substring(71, 78);
+      var signaltp1 = JSON.stringify(pages.slice(3, -3)).substring(125, 132);
+      var signaltp2 = JSON.stringify(pages.slice(3, -3)).substring(178, 185);
+      var eursignalobj = {
+        'date': signaldate,
+        'order': signalorder,
+        'price': signalprice,
+        'stoploss': signalsl,
+        'takeprofit1': signaltp1,
+        'takeprofit2': signaltp2,
+      }
+      eurusd_signals.push(eursignalobj);
+      console.log(eurusd_signals);
     }
   });
 }
 
-RealDate();
 FetchFileName();
